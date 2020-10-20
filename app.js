9f4440378e3b1fcf98ad081e742df3eb42e504b9
Antoine-Flo <antoine.flouzat@gmail.com>
2020-10-20 15:05:51 +0200

    Add date.js module

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+
 const app = express();
 
-
-let items = ["Buy Food",
+const items = ["Buy Food",
     "Cook Food",
     "Eat Food"
 ];
 
-let workItems = [];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
@@ -19,15 +21,7 @@ app.use(express.static('public'));
 
 app.get("/", function (req, res) {
 
-    let today = new Date();
-    let option = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", option)
-
+    const day = date.getDay();
 
     res.render("list", {
         listTitle: day,
@@ -52,7 +46,7 @@ app.get("/about", function (req, res) {
 
 app.post("/", function (req, res) {
 
-    let item = req.body.newItem
+    const item = req.body.newItem
 
     if (req.body.list === "Work") {
         workItems.push(item);
